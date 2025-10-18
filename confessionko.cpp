4f211fd7452872b648377ed6fd7148343a1d5686
mytommihatdog-ops <mytommihatdog@gmail.com>
2025-10-18 20:28:49 +0800

    Create confessionko.cpp

---
+++ confessionko.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string yourName = "Shan";
+    string crushName = "Myisha";
+    string message = "Myisha, gusto sana sabihen na gusto kita. Dati kala ko happy crush lang di naman tatagal—"
+                     "pero habang tumatagal mas lalo ko nare-realize na gusto kita..... "
+                     "I don't care if you don't like me back. I just wish you all the best.";
+    cout << "Hi " << crushName << ",\n";
+    cout << message << "\n";
+    cout << "- From, " << yourName << endl;
+    return 0;
+}
+added my love Confession
